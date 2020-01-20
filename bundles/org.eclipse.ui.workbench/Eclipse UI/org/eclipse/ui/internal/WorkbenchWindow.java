5a9f1add7659b44223d46fd021f0d9e47dd2d648
Mariia Mytrofanova <Mariia.Mytrofanova@halliburton.com>
2020-01-20 15:06:33 +0200

    Bug 549059 - RAT triggered after disposing the context under certain conditions

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchWindow.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchWindow.java
@@ -722,16 +722,18 @@ public class WorkbenchWindow implements IWorkbenchWindow {
 				RunAndTrack menuChangeManager = new RunAndTrack() {
 					@Override
 					public boolean changed(IEclipseContext context) {
-						ExpressionInfo info = new ExpressionInfo();
-						IEclipseContext leafContext = windowContext.getActiveLeaf();
-						MenuManagerRendererFilter.collectInfo(info, mainMenu, renderer, leafContext, true);
-						// if one of these variables change, re-run the RAT
-						for (String name : info.getAccessedVariableNames()) {
-							leafContext.get(name);
-						}
-						if (canUpdateMenus && workbench.getDisplay() != null) {
-							canUpdateMenus = false;
-							workbench.getDisplay().asyncExec(menuUpdater);
+						if (manageChanges) {
+							ExpressionInfo info = new ExpressionInfo();
+							IEclipseContext leafContext = windowContext.getActiveLeaf();
+							MenuManagerRendererFilter.collectInfo(info, mainMenu, renderer, leafContext, true);
+							// if one of these variables change, re-run the RAT
+							for (String name : info.getAccessedVariableNames()) {
+								leafContext.get(name);
+							}
+							if (canUpdateMenus && workbench.getDisplay() != null) {
+								canUpdateMenus = false;
+								workbench.getDisplay().asyncExec(menuUpdater);
+							}
 						}
 						return manageChanges;
 					}
