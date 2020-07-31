69eadd2541d2122748872ac35daa4acaaeba33c3
TLungol <Taras.Lungol@halliburton.com>
2020-07-31 13:05:28 +0300

    fix hadnles leak

--- bundles/org.eclipse.jface/src/org/eclipse/jface/resource/SvgImageDescriptor.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/resource/SvgImageDescriptor.java
@@ -197,4 +197,36 @@ public class SvgImageDescriptor extends ImageDescriptor {
 		}
 		return null;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + renderHeight;
+		result = prime * result + renderWidth;
+		result = prime * result + ((url == null) ? 0 : url.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SvgImageDescriptor other = (SvgImageDescriptor) obj;
+		if (renderHeight != other.renderHeight)
+			return false;
+		if (renderWidth != other.renderWidth)
+			return false;
+		if (url == null) {
+			if (other.url != null)
+				return false;
+		} else if (!url.equals(other.url))
+			return false;
+		return true;
+	}
+
 }
